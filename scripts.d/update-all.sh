a22cc68c12c4151e2731e82ba38c69e88367d274
brandon <brandon@smith.net>
2023-07-26 20:38:11 +0100

    Autocommit from 2023-07-26 20:38:11
    Klipper on commit: 6ce2bd6
    
    Moonraker on commit: aebcd00
    
    Mainsail version: v2.6.1

--- scripts.d/update-all.sh
+++ scripts.d/update-all.sh
@@ -2,10 +2,10 @@ sudo service klipper stop
 cd ~/klipper
 git pull
 
-make clean KCONFIG_CONFIG=config.spider
-make menuconfig KCONFIG_CONFIG=config.spider
-make KCONFIG_CONFIG=config.spider
-./scripts/flash-sdcard.sh /dev/serial/by-id/usb-Klipper_stm32f446xx_35001C000750305538333620-if00 fysetc-spider-v1
+make clean KCONFIG_CONFIG=config.octopus
+make menuconfig KCONFIG_CONFIG=config.octopus
+make KCONFIG_CONFIG=config.octopus
+./scripts/flash-sdcard.sh /dev/serial/by-id/usb-Klipper_stm32f446xx_310030000550314D35323820-if00 btt-octopus-f446-v1.1
 
 make clean KCONFIG_CONFIG=config.rpi
 make menuconfig KCONFIG_CONFIG=config.rpi
